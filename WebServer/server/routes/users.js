6217eed49f86e1d3d4a96efdc2bc2a76435ba23a
Marko Petrovic <mpetrovic.1684@gmail.com>
2022-10-29 18:10:28 +0200

    Update users.js

--- WebServer/server/routes/users.js
+++ WebServer/server/routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const controller = require("../controllers/controller.js")
 
 const router = express.Router();
+
+router.get('/izmenaLokaicje', controller.izmeniPodatke);
 
 module.exports = router;
