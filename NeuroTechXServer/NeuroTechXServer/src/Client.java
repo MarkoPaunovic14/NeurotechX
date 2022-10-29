6c58fcc3e07c79067f549c275e5c08a7860ccfb8
Marko Petrovic <mpetrovic.1684@gmail.com>
2022-10-29 15:29:02 +0200

    Promenjen kostur

--- NeuroTechXServer/NeuroTechXServer/src/Client.java
+++ NeuroTechXServer/NeuroTechXServer/src/Client.java
@@ -1,54 +1,25 @@
-import java.net.*;
-import java.io.*;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Client {
-
-    // socket initialization
-
-    private Socket socket = null;
-    private DataInputStream input = null;
-    private DataOutputStream out = null;
-
-    public Client(String address, int port){
-
-        try{
-            socket = new Socket(address, port);
-            System.out.println("Connected");
-
-            input = new DataInputStream(System.in);
-
-            out = new DataOutputStream(socket.getOutputStream());
-        }
-        catch(UnknownHostException u){
-            System.out.println(u);
-        }
-        catch(IOException i){
-            System.out.println(i);
-        }
-
-        String line = "";
-
-        while (!line.equals("Over")){
-            try{
-                line = input.readLine();
-                out.writeUTF(line);
-            }
-            catch (IOException i){
-                System.out.println(i);
-            }
-        }
-        try{
-            input.close();
-            out.close();
-            socket.close();
-        }
-        catch(IOException i){
-            System.out.println(i);
-        }
-    }
-
-
+class client {
     public static void main(String args[]){
-        Client client = new Client("127.0.0.1", 5001);
+        String message;
+        Scanner sc = new Scanner(System.in);
+        try{
+            Socket socket = new Socket("localhost", 5000);
+
+            System.out.println("Connected with Server...");
+            System.out.println("enter a message: ");
+            message = sc.nextLine();
+
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+            dout.writeUTF(message);
+            dout.flush();
+            dout.close();
+            socket.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
