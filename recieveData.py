3248e3ad12596b27103609634e15dae6a98c73bb
Marko Petrovic <70198333+marko1684@users.noreply.github.com>
2022-10-31 13:18:29 +0100

    Update recieveData.py

--- recieveData.py
+++ recieveData.py
@@ -4,18 +4,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 def main():
-    # first resolve an EEG stream on the lab network
     print("looking for an EEG stream...")
     streams = resolve_stream('type', 'EEG_1')
 
-    # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
 
     boolValue = 1
 
     while True:
-        # get a new sample (you can also omit the timestamp part if you're not
-        # interested in it)
         sample, timestamp = inlet.pull_sample()
         # print(sample[0])
         if sample[0] > 0.85 and boolValue:
